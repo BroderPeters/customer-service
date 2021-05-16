7fea552af30f37a806dd932db34a68b1f91c0e70
Broder Peters <contact@broderpeters.de>
2021-05-17 00:18:51 +0200

    Add automatic migration and finish up dependency injection in startup

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,8 +1,14 @@
+using CodeChallenge.CustomerService.Infrastructure.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CodeChallenge.CustomerService.Customers;
+using CodeChallenge.CustomerService.Repositories;
+using CodeChallenge.CustomerService.Services;
+using HotChocolate.Types;
 
 namespace CodeChallenge.CustomerService
 {
@@ -18,8 +24,18 @@ namespace CodeChallenge.CustomerService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddPooledDbContextFactory<CustomerDbContext>(options => options.UseSqlServer("Server=localhost;Database=master;User Id=sa;Password=Your_strong_password1;"));
+            services.AddScoped<CustomerDbContext>(p => p.GetRequiredService<IDbContextFactory<CustomerDbContext>>().CreateDbContext());
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ICustomerService, CustomerService.Services.CustomerService>();
 
-            services.AddControllers();
+            services.AddGraphQLServer()
+                .AddType<CustomerStatusType>()
+                .AddQueryType<CustomerQueriesType>()
+                .AddMutationType<CustomerMutationsType>()
+                .AddType<EmailAddressType>()
+                .AddType<NonEmptyStringType>()
+                .AddProjections();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,16 +46,11 @@ namespace CodeChallenge.CustomerService
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapGraphQL();
+                });
         }
     }
 }
